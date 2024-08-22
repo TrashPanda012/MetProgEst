e058cc226ff75ae0cae53a5c988ff7cf6dec322d
TrashPanda012 <kalbrowne@uamv.edu.ni>
2024-08-22 17:45:05 -0600

    segundo ejercicio

---
+++ Visual Studio/ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaración de variables;
+            string nombre;
+            //Leer el nombre y saludar
+            Console.WriteLine("Bienvenido!");
+            Console.Write("Dime tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre + ", mucho gusto!");
+        }
+    }
+}
