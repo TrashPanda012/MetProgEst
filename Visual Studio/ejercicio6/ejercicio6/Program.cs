f887f8d2de9a47cd3873032a6123164a57fa9f6e
TrashPanda012 <kalbrowne@uamv.edu.ni>
2024-08-22 19:01:21 -0600

    sexto ejercicio

---
+++ Visual Studio/ejercicio6/ejercicio6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ejercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracion de variables
+            int numero;
+            //Peticion de datos
+            Console.WriteLine("Bienvenido al programa de verificacion de pares e impares!");
+            Console.Write("Dime el numero a verificar si es par o impar: ");
+            numero = int.Parse(Console.ReadLine());
+            //Verificacion
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El numero " + numero + " es par!");
+            }
+            else
+            {
+                Console.WriteLine("El numero " + numero + " es impar!");
+            }
+        }
+    }
+}
