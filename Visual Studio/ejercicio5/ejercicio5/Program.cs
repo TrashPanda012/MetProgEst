2dd60cdbb17662978c1953fbcf6e658dee66520f
TrashPanda012 <kalbrowne@uamv.edu.ni>
2024-08-22 18:47:25 -0600

    quinto ejercicio

---
+++ Visual Studio/ejercicio5/ejercicio5/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ejercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracion variables
+            double salario, nuevoSalario;
+            string cargo, nombre;
+            //Peticion de datos
+            Console.WriteLine("Bienvenido al sistema de cálculo de nuevo salario!");
+            Console.Write("Escriba su nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("Ahora escriba su cargo: ");
+            cargo = Console.ReadLine();
+            Console.Write("Escriba su salario actual: ");
+            salario = double.Parse(Console.ReadLine());
+            //Calculo de nuevo salario
+            nuevoSalario = salario * 1.10;
+            //Impresion de datos de usuario
+            Console.WriteLine("Su nombre es: " + nombre);
+            Console.WriteLine("Su cargo es: " + cargo);
+            Console.WriteLine("Su nuevo salario es: " + nuevoSalario);
+        }
+    }
+}
