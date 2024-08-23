c70fac8d95ae1a6b1c660ccc1d1508d48d91a7b3
TrashPanda012 <kalbrowne@uamv.edu.ni>
2024-08-22 18:20:59 -0600

    cuarto ejercicio

---
+++ Visual Studio/ejercicio4/ejercicio4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracion de variables
+            double radio, resultado;
+
+            //Peticion de datos
+            Console.Write("Dime el radio del círculo: ");
+            radio = double.Parse(Console.ReadLine());
+            //Formula y resultado
+            resultado = Math.PI * Math.Pow(2, radio);
+            Console.WriteLine("El área del círculo es igual a: " + resultado);
+        }
+    }
+}
